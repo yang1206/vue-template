c2ff8822cb33da52f22ace5844d1304e1bf105d5
yang1206 <yang15188280673@outlook.com>
2022-10-07 22:32:28 +0800

    fix: 🧩 删除多余配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import Unocss from 'unocss/vite'
 const pathSrc = path.resolve(__dirname, 'src')
 export default defineConfig({
@@ -17,13 +16,13 @@ export default defineConfig({
     }),
     Components({
       dts: path.resolve(pathSrc, 'typings/components.d.ts'),
-      resolvers: [ElementPlusResolver()],
     }),
     Unocss(),
   ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '~': path.resolve(__dirname, './'),
     },
   },
 })
