f6599a6d2e98b37c848e7422111025d045854efd
yang1206 <yang15188280673@outlook.com>
2023-02-26 18:01:52 +0800

    fix: counter

--- src/views/Count/index.vue
+++ src/views/Count/index.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { useCounterStore } from '@/store'
 const useCount = useCounterStore()
-const { count, inc, dec } = useCount
+const { inc, dec } = useCount
 </script>
 
 <template>
   <div>
     <p class="font-semibold">
-      {{ count }}
+      {{ useCount.count }}
     </p>
     <button class="btn m-4 h-8 w-8 rounded-full text-center" @click="inc()">
       +
