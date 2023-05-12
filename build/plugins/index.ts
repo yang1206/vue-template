820371e771241e1cc462755f82ee01655548bca8
yang1206 <yang15188280673@outlook.com>
2023-05-12 22:23:58 +0800

    chore: up deps

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -1,10 +1,13 @@
 import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import unocss from 'unocss/vite'
+import VueDevTools from 'vite-plugin-vue-devtools'
 import unplugins from './unplugin'
+
 export function setupVitePlugins(viteEnv?: ViteEnv, isBuild?: boolean): PluginOption[] {
   const plugins = [
     ...unplugins,
+    VueDevTools(),
     vue(),
     unocss(),
 
