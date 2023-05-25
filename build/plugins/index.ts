76d08665f875840f9b68e16998a5c8c38eb3505d
yang1206 <yang15188280673@outlook.com>
2023-05-25 14:23:49 +0800

    fix: types

--- build/plugins/index.ts
+++ build/plugins/index.ts
@@ -4,7 +4,7 @@ import unocss from 'unocss/vite'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import unplugins from './unplugin'
 
-export function setupVitePlugins(viteEnv?: ViteEnv, isBuild?: boolean): PluginOption[] {
+export function setupVitePlugins(viteEnv?: ImportMetaEnv, isBuild?: boolean): PluginOption[] {
   const plugins = [
     ...unplugins,
     VueDevTools(),
