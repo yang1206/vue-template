abbf8fff1b987aa8206af1f0c4366747765e5bf5
yang1206 <yang15188280673@outlook.com>
2023-06-10 21:38:37 +0800

    chore: up deps

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,19 +10,23 @@ import {
 } from 'unocss'
 
 export default defineConfig({
-  exclude: [
-    'node_modules',
-    '.git',
-    '.github',
-    '.husky',
-    '.vscode',
-    'build',
-    'dist',
-    'mock',
-    'public',
-    'types',
-    './stats.html',
-  ],
+  content: {
+    pipeline: {
+      exclude: [
+        'node_modules',
+        '.git',
+        '.github',
+        '.husky',
+        '.vscode',
+        'build',
+        'dist',
+        'mock',
+        'public',
+        'types',
+        './stats.html',
+      ],
+    },
+  },
   shortcuts: [
     ['btn', 'rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
     ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
